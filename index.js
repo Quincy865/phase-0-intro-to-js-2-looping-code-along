9f6b96910ab994cdce3785ab40a0cf0e376d0040
Quincy Michael <michaelkirima865@gmail.com>
2024-09-19 13:07:41 +0300

    Adds looping js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+const names = ["Quincy", "Bob", "Sean"];
+const event = "birthday";
+const thankYouCards = writeCards(names, event);
+console.log(thankYouCards);
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+
+countDown(5);
